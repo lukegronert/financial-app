e31a46434edfd8a383fc27c9f9073881724f7f89
Luke Gronert <lukegronert@gmail.com>
2022-11-19 01:30:18 +0800

    Added Algolia. Added the Algolia SearchBox and Hits to Explore. Currently styling the Hits. Added styling in globals.css to target the hits and make them show in a grid.

---
+++ components/Hit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hit = ({hit}) => {
+  return (
+    <div>{hit.name}</div>
+  )
+}
+
+export default Hit
--- pages/Explore.js
+++ pages/Explore.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ExploreInstrumentDetail from '../components/ExploreInstrumentDetail';
+import { useRouter } from 'next/navigation';
+
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import 'instantsearch.css/themes/reset.css';
+import 'instantsearch.css/themes/satellite.css';
 
 import { RiCloseLine } from 'react-icons/ri'
 
@@ -47,22 +53,34 @@ const dummyData = [
     },
 ];
 
+const searchClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+  process.env.NEXT_PUBLIC_ALGOLIA_API_KEY,
+);
+
 const Explore = () => {
+  const router = useRouter();
 
   return (
-    <div className="bg-explore-gray h-full">
-      <div className="h-1/4 bg-white rounded-t-xl px-3">
-        <div className="flex flex-row justify-between items-center py-3">
+    <div className="h-max flex flex-col bg-explore-gray">
+      <div className="bg-white rounded-t-xl">
+        <div className="flex flex-row justify-between items-center py-3 px-3 rounded-t-xl">
           <h1 className="text-explore-blue font-extrabold text-3xl">
             Welcome!
           </h1>
           <RiCloseLine size='2em' />
         </div>
-        <p className="font-bold text-gray-400">Choose your interests to follow and trade on your terms.</p>
-        <input type="text" />
+        <p className="font-bold text-gray-400 px-3 mb-3">Choose your interests to follow and trade on your terms.</p>
+        <InstantSearch searchClient={searchClient} indexName="financial-instruments">
+          <SearchBox className="mb-3 px-3" />
+          <div className="bg-explore-gray grow">
+            <div className="h-full text-center p-3">
+              <Hits hitComponent={ExploreInstrumentDetail} onClick={() => router.push('/')} />
+            </div>
+          </div>
+        </InstantSearch>
       </div>
-      <p></p>
-      <div className="h-screen">
+      {/* <div className="h-screen">
         <div className="grid grid-cols-2 gap-4 justify-center items-center flex-wrap h-full p-3">
           {dummyData.map((brand) => (
             <div className="basis-1/2 flex justify-center items-center text-center h-full rounded-lg bg-white" key={brand.name}>
@@ -70,7 +88,7 @@ const Explore = () => {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }

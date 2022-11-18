1b064cc350144d69c900481a93c76e5f564383fc
Luke Gronert <lukegronert@gmail.com>
2022-11-18 17:01:44 +0800

    Added key to divs created from dummyData.map in Explore page

--- pages/Explore.js
+++ pages/Explore.js
@@ -42,8 +42,8 @@ const Explore = () => {
       <div className="h-screen">
         <div className="grid grid-cols-2 gap-4 justify-center items-center flex-wrap h-4/5 p-3 bg-explore-gray">
           {dummyData.map((brand) => (
-            <div className="basis-1/2 flex justify-center items-center text-center h-full rounded-lg bg-white">
-              <ExploreInstrumentDetail name={brand.name} logo={brand.logo} key={brand.name} />
+            <div className="basis-1/2 flex justify-center items-center text-center h-full rounded-lg bg-white" key={brand.name}>
+              <ExploreInstrumentDetail name={brand.name} logo={brand.logo} />
             </div>
           ))}
         </div>

6f9fac8ce2b175825bcfc241f6c61ff90bcdf2ae
Luke Gronert <lukegronert@gmail.com>
2022-11-19 14:42:08 +0800

    Added styling to globals.css to override Algolia styling on Hits. Added conditional rendering for the Welcome/Search box that switches between Welcome bar with search, and Dashboard.

--- components/ExploreInstrumentDetail.js
+++ components/ExploreInstrumentDetail.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const ExploreInstrumentDetail = ({ hit }) => {
 
   return (
     <div className="flex flex-col w-full h-full justify-between">
+        {/* Adds logo centered in circle border as shown in challenge pictures */}
         {/* <div className="flex justify-center items-center self-center rounded-full border h-16 w-16 p-3 mb-2">
-            <div className="h-10 w-10 flex justify-center items-center">
-                <img src={logo.src} alt={`${name} logo`} className="max-h-10 w-10" />
+              <div className="h-10 w-10 flex justify-center items-center">
+                  <img src={logo.src} alt={`${name} logo`} className="max-h-10 w-10" />
             </div>
         </div> */}
-        <p className="font-extrabold text-xl text-explore-blue">{hit.name}</p>
-        <button className="bg-explore-blue text-white font-bold self-center w-10/12 p-2 rounded-lg">Follow</button>
+        <p className="font-extrabold text-lg text-explore-blue">{hit.name.length > 25 ? `${hit.name.slice(0,25)}...` : hit.name}</p>
+        <button className="bg-explore-blue text-white font-bold self-center w-10/12 p-2 rounded-lg translate-y-3">Follow</button>
     </div>
   )
 }
--- pages/Explore.js
+++ pages/Explore.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExploreInstrumentDetail from '../components/ExploreInstrumentDetail';
-import { useRouter } from 'next/navigation';
 
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
@@ -8,50 +7,7 @@ import 'instantsearch.css/themes/reset.css';
 import 'instantsearch.css/themes/satellite.css';
 
 import { RiCloseLine } from 'react-icons/ri'
-
-import appleLogo from '../assets/logos/apple.svg';
-import gamestopLogo from '../assets/logos/gamestop.svg';
-import disneyLogo from '../assets/logos/disney.svg';
-import teslaLogo from '../assets/logos/tesla.svg';
-import nikeLogo from '../assets/logos/nike.svg';
-import amazonLogo from '../assets/logos/amazon.svg';
-import netflixLogo from '../assets/logos/netflix.svg';
-import amcLogo from '../assets/logos/amc.svg';
-
-const dummyData = [
-    {
-        name: 'Apple',
-        logo: appleLogo
-    },
-    {
-        name: 'GameStop',
-        logo: gamestopLogo
-    },
-    {
-        name: 'Disney',
-        logo: disneyLogo
-    },
-    {
-        name: 'Tesla',
-        logo: teslaLogo
-    },
-    {
-        name: 'Nike',
-        logo: nikeLogo
-    },
-    {
-        name: 'Amazon',
-        logo: amazonLogo
-    },
-    {
-        name: 'Netflix',
-        logo: netflixLogo
-    },
-    {
-        name: 'AMC',
-        logo: amcLogo
-    },
-];
+import { FiSearch } from 'react-icons/fi'
 
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
@@ -59,27 +15,38 @@ const searchClient = algoliasearch(
 );
 
 const Explore = () => {
-  const router = useRouter();
+  const [openSearch, setOpenSearch] = useState(true);
 
   return (
-    <div className="h-max flex flex-col bg-explore-gray">
-      <div className="bg-white rounded-t-xl">
-        <div className="flex flex-row justify-between items-center py-3 px-3 rounded-t-xl">
-          <h1 className="text-explore-blue font-extrabold text-3xl">
-            Welcome!
-          </h1>
-          <RiCloseLine size='2em' />
-        </div>
-        <p className="font-bold text-gray-400 px-3 mb-3">Choose your interests to follow and trade on your terms.</p>
-        <InstantSearch searchClient={searchClient} indexName="financial-instruments">
-          <SearchBox className="mb-3 px-3" />
-          <div className="bg-explore-gray grow">
-            <div className="h-full text-center p-3">
-              <Hits hitComponent={ExploreInstrumentDetail} onClick={() => router.push('/')} />
-            </div>
+    <div className="h-screen flex flex-col bg-explore-gray">
+      {openSearch ? (
+        <div className="bg-white rounded-t-xl">
+          <div className="flex flex-row justify-between items-center py-3 px-3 rounded-t-xl">
+            <h1 className="text-explore-blue font-extrabold text-3xl">
+              Welcome!
+            </h1>
+            <RiCloseLine size='1.5em' onClick={() => setOpenSearch(false)} />
           </div>
-        </InstantSearch>
-      </div>
+          <p className="font-bold text-gray-400 px-3 mb-3">Choose your interests to follow and trade on your terms.</p>
+          <InstantSearch searchClient={searchClient} indexName="financial-instruments">
+            <SearchBox className="mb-3 px-3" />
+            <div className="bg-explore-gray grow w-screen">
+              <div className="h-full text-center p-3">
+                <Hits hitComponent={ExploreInstrumentDetail} />
+              </div>
+            </div>
+          </InstantSearch>
+        </div>
+      ) : (
+        <div className="bg-white rounded-t-xl">
+          <div className="flex flex-row justify-between items-center py-3 px-3 rounded-t-xl">
+              <h1 className="text-explore-blue font-extrabold text-3xl">
+                Dashboard
+              </h1>
+              <FiSearch size='1.5em' onClick={() => setOpenSearch(true)} />
+            </div>
+        </div>
+      )}
       {/* <div className="h-screen">
         <div className="grid grid-cols-2 gap-4 justify-center items-center flex-wrap h-full p-3">
           {dummyData.map((brand) => (

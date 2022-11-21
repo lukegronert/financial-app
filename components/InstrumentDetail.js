5af785c60d7282d88d957c83b515071900f12380
Luke Gronert <lukegronert@gmail.com>
2022-11-21 17:18:37 +0800

    Switched IBMIntraDay data to 30min interval. Worked out logic for all displayed values in InstrumentDetail.

--- components/InstrumentDetail.js
+++ components/InstrumentDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { IoIosArrowRoundBack } from "react-icons/io";
@@ -6,10 +6,44 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import { AiFillStar } from "react-icons/ai";
 import { FiShare } from "react-icons/fi";
 
+import data from "../mockData/IBMIntraDay";
+
 const InstrumentDetail = () => {
+  const [plusMinus, setPlusMinus] = useState("+");
+  const [changePercentage, setChangePercentage] = useState(null);
+
   const router = useRouter();
   const { instrumentName, instrumentSymbol } = router.query;
 
+  const timeData = Object.entries(data["Time Series (30min)"]);
+  console.log(timeData)
+  console.log(timeData[0][0].split(" ")[0]);
+  const dayData = Object.values(timeData).filter(
+    (obj) => obj[0].split(" ")[0] === timeData[0][0].split(" ")[0]
+  );
+  console.log("day data", dayData);
+  const currentValue = Number(dayData[0][1]["4. close"]).toFixed(2);
+  const changeValue = (
+    ((currentValue - Number(dayData[dayData.length - 1][1]["4. close"])) *
+      100) /100).toFixed(2);
+
+  const getChangePercentage = () => {
+    let change = ((changeValue / currentValue) * 100).toFixed(2);
+    change.replace('-', '');
+    setChangePercentage(change)
+  }
+
+  console.log("current", currentValue);
+  console.log("changeValue", changeValue);
+  console.log("differencePercentage", changePercentage);
+
+  useEffect(() => {
+    if(changeValue > 0) {
+        setPlusMinus('-')
+    }
+    getChangePercentage()
+  }, [])
+  
   return (
     <div>
       <div>
@@ -29,18 +63,18 @@ const InstrumentDetail = () => {
           <h1>{instrumentName}</h1>
         </div>
         <div>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span>{`$${currentValue}`}</span>
+          <span>{`${plusMinus} ${changePercentage}%`}</span>
+          <span>{`${plusMinus} $${changeValue}`}</span>
         </div>
         <div>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+          <button>1d</button>
+          <button>5d</button>
+          <button>30d</button>
+          <button>90d</button>
+          <button>6m</button>
+          <button>1y</button>
+          <button>All</button>
         </div>
         <div>GRAPH</div>
         <div>
@@ -49,11 +83,11 @@ const InstrumentDetail = () => {
       </div>
       <div>
         <div>
-            <p>News</p>
-            <span>
-                See all
-                <IoIosArrowRoundForward />
-            </span>
+          <p>News</p>
+          <span>
+            See all
+            <IoIosArrowRoundForward />
+          </span>
         </div>
       </div>
     </div>

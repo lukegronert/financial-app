b04a93c0f89abc399ad661d303322af761aea12c
Luke Gronert <lukegronert@gmail.com>
2022-11-21 13:34:38 +0800

    Set up dynamic routing in pages/instruments. Created json file mockData to use as example for setting up graphing.

--- components/InstrumentDetail.js
+++ components/InstrumentDetail.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { AiFillStar } from "react-icons/ai";
+import { FiShare } from "react-icons/fi";
 
 const InstrumentDetail = () => {
+  const router = useRouter();
+  const { instrumentName, instrumentSymbol } = router.query;
+
   return (
-    <div>InstrumentDetail</div>
-  )
-}
+    <div>
+      <div>
+        <div>
+          <div>
+            <IoIosArrowRoundBack />
+          </div>
+          <div>
+            <AiFillStar />
+            <FiShare />
+          </div>
+        </div>
+        <div>
+          <p>{instrumentSymbol}</p>
+        </div>
+        <div>
+          <h1>{instrumentName}</h1>
+        </div>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div>GRAPH</div>
+        <div>
+          <button>FOLLOW</button>
+        </div>
+      </div>
+      <div>
+        <div>
+            <p>News</p>
+            <span>
+                See all
+                <IoIosArrowRoundForward />
+            </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default InstrumentDetail;
---
+++ pages/instruments/[instrumentName]/[instrumentSymbol].js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InstrumentDetail from '../../../components/InstrumentDetail';
+
+const InstrumentId = () => {
+
+  return (
+    <div>
+      <InstrumentDetail />
+    </div>
+  )
+}
+
+export default InstrumentId;

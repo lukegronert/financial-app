377a7366e27f81a85f2227057ea7d244fdcd10a4
Luke Gronert <lukegronert@gmail.com>
2022-11-23 23:48:28 +0800

    Removed dynamic and Suspense from /instruments/[instrumentName]/[instrumentSymbol]

--- pages/instruments/[instrumentName]/[instrumentSymbol].js
+++ pages/instruments/[instrumentName]/[instrumentSymbol].js
@@ -1,21 +1,10 @@
 import React, { Suspense } from "react";
-import dynamic from "next/dynamic";
-
 import InstrumentDetail from "../../../components/InstrumentDetail";
-
-const DynamicInstrumentDetail = dynamic(
-  () => import("../../../components/InstrumentDetail"),
-  {
-    ssr: false,
-  }
-);
 
 const InstrumentId = () => {
   return (
     <div>
-      <Suspense fallback={`Loading...`}>
-        <DynamicInstrumentDetail />
-      </Suspense>
+      <InstrumentDetail />
     </div>
   );
 };

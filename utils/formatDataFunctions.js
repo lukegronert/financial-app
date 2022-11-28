c3ab6c283024822d7399248bcaab3f92e53bd908
Luke Gronert <lukegronert@gmail.com>
2022-11-28 19:25:27 +0800

    Refactored formatDateFunctions.

--- utils/formatDataFunctions.js
+++ utils/formatDataFunctions.js
@@ -1,16 +1,12 @@
 const formatLocalUSD = (value) => {
-  // using undefined formats the number using the browser's locale settings
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
-  // a user in Germany will see 25,95 $
-  // while a user in the US will see $25.95
-  return value.toLocaleString(undefined, {
+  return value.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
 };
 
 const formatLocalPercentage = (value) => {
-  return value.toLocaleString(undefined, {
+  return value.toLocaleString("en-US", {
     style: "percent",
     minimumFractionDigits: 2,
   });

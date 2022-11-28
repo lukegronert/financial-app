becc83889248e6c8fd61b00601f525a7a6004c39
Luke Gronert <lukegronert@gmail.com>
2022-11-28 17:40:55 +0800

    Renamed watchList page.

--- pages/watchList.js
+++ pages/watchList.js
@@ -1,8 +1,8 @@
 import React from "react";
 import WatchList from "../components/WatchList";
 
-const UserId = () => {
+const WatchListPage = () => {
   return <WatchList limit={50} seeAll={false} backButton={true} />;
 };
 
-export default UserId;
+export default WatchListPage;
